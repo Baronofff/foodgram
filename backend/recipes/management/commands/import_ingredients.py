d56933412459b35c3277f10deb97b9130b4aa4e7
Baronofff <Baronov.ev@gmail.com>
2025-06-21 21:33:10 +0300

    trytry

--- backend/recipes/management/commands/import_ingredients.py
+++ backend/recipes/management/commands/import_ingredients.py
@@ -22,8 +22,8 @@ class Command(BaseCommand):
             added = 0
             skipped = 0
             for row in reader:
-                name = row[0]
-                unit = row[1]
+                name = row['name']
+                unit = row['measurement_unit']
                 obj, created = Ingredient.objects.get_or_create(
                     name=name, measurement_unit=unit
                 )
